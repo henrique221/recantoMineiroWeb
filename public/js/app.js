c7847552f841abc593d01e99815afc3565010f66
henrique221 <hborges9294@gmail.com>
2019-04-06 06:17:55 -0300

    Melhorando front

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
-require('../css/app.css');
+$(document).ready(function() {
+
+    $('input').focus(function () {
+        $(this).parent().find(".label-txt").addClass('label-active');
+    });
+
+    $("input").focusout(function () {
+        if ($(this).val() == '') {
+            $(this).parent().find(".label-txt").removeClass('label-active');
+        }
+        ;
+    });
+})
